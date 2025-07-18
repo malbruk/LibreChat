c04aa365c8c3d60e7f732bb6f9787704896e3ee0
malbruk <malka2039@gmail.com>
2025-07-18 04:00:59 +0300

    mabruk css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './locales/i18n';
 import App from './App';
 import './style.css';
+import './mabruk.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
 import 'katex/dist/katex.min.css';
